bf66a2344397881e2be8240067dfe7ab99adb116
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-11-01 23:00:27 +0100

    experimenting external project find/build with a cmake function

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -79,3 +79,77 @@ function(find_python_module module)
     endif(NOT PY_${module_upper})
     find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
 endfunction(find_python_module)
+
+
+
+#need a location outside build ?
+set(DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/third-party/downloads")
+
+include(CMakeParseArguments)
+
+set(ZLIB_URL  "http://sourceforge.net/projects/libpng/files/zlib/1.2.8/zlib-1.2.8.tar.gz/download")
+set(ZLIB_MD5  "44d667c142d7cda120332623eab69f40")
+#could there be a default?
+set(ZLIB_DOWNLOAD_NAME "zlib-1.2.8.tar.gz")
+
+set(CURL_URL  "http://curl.haxx.se/download/curl-7.40.0.tar.gz")
+set(CURL_MD5  "58943642ea0ed050ab0431ea1caf3a6f")
+set(CURL_DOWNLOAD_NAME "curl-7.40.0.tar.gz")
+
+#erg.. no cmake build
+set(OPENSSL_URL  "https://github.com/openssl/openssl/archive/OpenSSL_1_0_1e.zip")
+set(OPENSSL_MD5  "de0f06b07dad7ec8b220336530be1feb")
+set(OPENSSL_DOWNLOAD_NAME "OpenSSL_1_0_1e.zip")
+
+#need a better way. discuss with dimitry
+set(ZLIB_INSTALL_DIR "${CMAKE_BINARY_DIR}/third-party/Install/ZLIB")
+set(CURL_INSTALL_DIR "${CMAKE_BINARY_DIR}/third-party/Install/CURL")
+
+set(OPENSSL_INSTALL_DIR "${CMAKE_BINARY_DIR}/third-party/Install/OPENSSL")
+
+function(configure_external_project)
+  cmake_parse_arguments(PKG "" "NAME" "DEPENDS" ${ARGN} )
+  option(WITH_${PKG_NAME} "Set ON to use ${PKG_NAME}" ON)
+
+  if(WITH_${PKG_NAME})
+    foreach(DEPEND ${PKG_DEPENDS})
+      if(NOT TARGET ${DEPEND})
+        configure_external_project(NAME ${DEPEND})
+        set(${PKG_NAME}_CMAKE_CONFIG "-D${DEPEND}_INCLUDE_DIRS=${GDAL_${DEPEND}_INCLUDE_DIRS} -D${DEPEND}_LIBRARIES=${GDAL_${DEPEND}_LIBRARIES}")
+      endif()
+    endforeach()
+
+    find_package(${PKG_NAME})
+
+    if(${PKG_NAME}_FOUND)
+      set(GDAL_${PKG_NAME}_INCLUDE_DIRS ${${PKG_NAME}_INCLUDE_DIRS} "internal cmake var to hold ${PKG_NAME}'s headers" INTERNAL)
+      set(GDAL_${PKG_NAME}_LIBRARIES ${${PKG_NAME}_PKGRARIES} "internal cmake var to hold ${PKG_NAME}'s lib" INTERNAL)
+    else()
+
+      FILE(READ "${CMAKE_SOURCE_DIR}/cmake/cache/${PKG_NAME}-cache.cmake" ${PKG_NAME}_CACHE_ARGS_LIST)
+      STRING(REGEX REPLACE ";" "\\\\;" ${PKG_NAME}_CACHE_ARGS_LIST "${${PKG_NAME}_CACHE_ARGS_LIST}")
+      STRING(REGEX REPLACE "\n" ";" ${PKG_NAME}_CACHE_ARGS_LIST "${${PKG_NAME}_CACHE_ARGS_LIST}")
+
+      list(APPEND ${PKG_NAME}_CACHE_ARGS_LIST -DCMAKE_INSTALL_PREFIX:PATH=${${PKG_NAME}_INSTALL_DIR})
+      list(APPEND ${PKG_NAME}_CACHE_ARGS_LIST ${${PKG_NAME}_CMAKE_CONFIG})
+      message(WARNING ${${PKG_NAME}_CACHE_ARGS_LIST})
+
+      #add externalproject
+        ExternalProject_Add(${PKG_NAME}
+          DEPENDS ${PKG_DEPENDS}
+          URL ${${PKG_NAME}_URL}
+          URL_MD5 ${${PKG_NAME}_MD5}
+          DOWNLOAD_NAME ${${PKG_NAME}_DOWNLOAD_NAME}
+          DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+          CMAKE_CACHE_ARGS ${${PKG_NAME}_CACHE_ARGS_LIST}
+          )
+
+        set(GDAL_${PKG_NAME}_INCLUDE_DIRS ${${PKG_NAME}_INSTALL_DIR}/include "internal cmake var to hold ${PKG_NAME}'s headers" INTERNAL)
+        set(GDAL_${PKG_NAME}_LIBRARIES ${${PKG_NAME}_INSTALL_DIR}/lib/libz.so "internal cmake var to hold ${PKG_NAME}'s lib" INTERNAL)
+      endif()
+
+      list(APPEND GDAL_TARGET_LINK_LIB ${GDAL_${PKG_NAME}_LIBRARIES})
+
+  endif()
+
+endfunction()
